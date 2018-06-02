f6a5c407296c1cc0d2e2e171717893ea7a9ee6fc
MikoJx823 <38484418+MikoJx823@users.noreply.github.com>
2018-06-02 13:22:52 +0800

    20180602v2

---
+++ src/main/java/com/project/dao/ConfigDao.java
@@ -0,0 +1,156 @@
+package com.project.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.project.bean.ConfigBean;
+import com.project.util.PropertiesUtil;
+import com.project.util.StaticValueUtil;
+import com.project.util.StringUtil;
+
+/*Function : 
+ * Add
+ * Edit
+ * Get single by id 
+ * Get List by sql, 
+ * Get List by sql with page 
+ * Get list by parent id
+ * Get total page
+ * 
+ * */
+
+public class ConfigDao extends GenericDao{
+	
+	private static Logger log = Logger.getLogger(ConfigDao.class);
+	private static ConfigDao instance = null;
+
+	public static synchronized ConfigDao getInstance(){
+		if (instance == null)
+			instance = new ConfigDao();
+		return instance;
+	}
+
+	public ConfigBean getBeanById(int id){
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "";
+		ConfigBean result = null;
+		List<ConfigBean> list = new ArrayList<ConfigBean>();
+		try
+		{
+			conn = ConectionFactory.getConnection();
+			sql = "select * from config where id=" + id ;
+
+			pstmt = new LoggableStatement(conn, sql);
+
+			//log.info("Executing SQL:" + ((LoggableStatement) pstmt).getQueryString());
+			
+			rs = pstmt.executeQuery();
+			
+			list = getConfigBeanFromResultSet(rs);
+			
+			if(list.size() > 0) {
+				result = list.get(0);
+			}			
+		}
+		catch (Exception e)
+		{
+			log.error(sql, e);
+			result = null;
+		}
+		finally
+		{
+			DbClose.closeAll(rs, pstmt, conn);
+		}
+		return result;
+	}
+	
+	public ConfigBean getBeanByName(String name){
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "";
+		ConfigBean result = null;
+		List<ConfigBean> list = new ArrayList<ConfigBean>();
+		try
+		{
+			conn = ConectionFactory.getConnection();
+			sql = "select * from config where name ='" + name + "'";
+
+			pstmt = new LoggableStatement(conn, sql);
+
+			//log.info("Executing SQL:" + ((LoggableStatement) pstmt).getQueryString());
+			
+			rs = pstmt.executeQuery();
+			
+			list = getConfigBeanFromResultSet(rs);
+			
+			if(list.size() > 0) {
+				result = list.get(0);
+			}			
+		}
+		catch (Exception e)
+		{
+			log.error(sql, e);
+			result = null;
+		}
+		finally
+		{
+			DbClose.closeAll(rs, pstmt, conn);
+		}
+		return result;
+	}
+	
+	public List<ConfigBean> getListBySqlwhere(String sqlWhere){
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "";
+		List<ConfigBean> result = new ArrayList<ConfigBean>();
+		try
+		{
+			conn = ConectionFactory.getConnection();
+			sql = "select * from config "+sqlWhere;
+
+			pstmt = new LoggableStatement(conn, sql);
+			//log.info("Executing SQL:" + ((LoggableStatement) pstmt).getQueryString());
+			
+			rs = pstmt.executeQuery();
+
+			result = getConfigBeanFromResultSet(rs);
+			
+		}
+		catch (Exception e)
+		{
+			log.error(sql, e);
+		}
+		finally
+		{
+			DbClose.closeAll(rs, pstmt, conn);
+		}
+		return result;
+	}
+	
+	private List<ConfigBean> getConfigBeanFromResultSet(ResultSet rs) throws Exception{
+
+		List<ConfigBean> result = new ArrayList<ConfigBean>();
+		while (rs != null && rs.next())
+		{
+			ConfigBean bean = new ConfigBean();
+			bean.setId(rs.getInt("id"));
+			bean.setName(rs.getString("name"));
+			bean.setValue(rs.getString("value"));
+			
+			result.add(bean);
+		}
+		return result;
+	}
+
+}
--- src/main/java/com/project/pulldown/CountryPulldown.java
+++ src/main/java/com/project/pulldown/CountryPulldown.java
@@ -1,6 +1,6 @@
 package com.project.pulldown;
 
-import com.project.util.StaticValueUtil;
+import com.project.util.StringUtil;
 
 public class CountryPulldown {
 	//https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
@@ -29,7 +29,8 @@ public class CountryPulldown {
 		result += "<option value=''>" + "--- Please Select --- " + "</option>\n";
 		
 		for(int i = 0 ; i < value.length; i++){	
-			if(value[i].equals(initValue)){
+			
+			if(value[i].equals(initValue) || (StringUtil.filter(initValue).equals("") && value[i].equals("MY")) ){
 				result += "<option value='" + value[i] + "' selected >" + name[i] + "</option>\n";  
 			}else{
 				result += "<option value='" + value[i] + "'>" + name[i] + "</option>\n";  
